1bb95bf023eccf5e7ae7482cca970f8274832762
Steven Lynn <i@stv.lol>
2023-10-05 01:31:21 +0800

    第一版

---
+++ config.js
@@ -0,0 +1,61 @@
+const siteConfig = {
+    title: "我的导航站点",
+    siteHeader: "欢迎来到我的导航站点",
+    header: "<!-- Here you can insert your Google Analytics or other scripts -->",
+    footer: {
+        icp: "粤ICP备XXXXX号",
+        copyright: "© 2023 我的导航站点"
+    },
+    backgroundImage: true,
+    categories: [
+        {
+            title: "分类1",
+            links: [
+                {
+                    text: "链接1",
+                    description: "描述1",
+                    href: "https://stv.lol",
+                    thumbnail: "img/example1.png"
+                },
+                {
+                    text: "链接1",
+                    description: "描述1",
+                    href: "https://example.com",
+                    thumbnail: "img/example1.png"
+                },
+                {
+                    text: "链接1",
+                    description: "描述1",
+                    href: "https://example.com",
+                    thumbnail: "img/example1.png"
+                }
+                // ...其他链接
+            ]
+        },
+        {
+            title: "分类2",
+            links: [
+                {
+                    text: "链接1",
+                    description: "描述1",
+                    href: "https://example.com",
+                    thumbnail: "img/example1.png"
+                },
+                {
+                    text: "链接1",
+                    description: "描述1",
+                    href: "https://example.com",
+                    thumbnail: "img/example1.png"
+                },
+                {
+                    text: "链接1",
+                    description: "描述1",
+                    href: "https://example.com",
+                    thumbnail: "img/example1.png"
+                }
+                // ...其他链接
+            ]
+        }
+        // ...其他分类
+    ]
+};
---
+++ scripts.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const body = document.body;
+    const icon = document.querySelector("#theme-toggle i");
+    document.getElementById("theme-toggle").addEventListener("click", function() {
+        if (body.classList.contains("light-mode")) {
+            body.classList.remove("light-mode");
+            body.classList.add("dark-mode");
+            icon.className = "fa-solid fa-moon";
+        } else {
+            body.classList.remove("dark-mode");
+            body.classList.add("light-mode");
+            icon.className = "fa-regular fa-sun";
+        }
+    });
+
+    const hour = new Date().getHours();
+    body.classList.add(hour >= 7 && hour < 19 ? "light-mode" : "dark-mode");
+
+    const main = document.querySelector("main");
+    
+    document.querySelector("header h1").textContent = siteConfig.siteHeader;
+    
+    const footerContent = `${siteConfig.footer.copyright} | ${siteConfig.footer.icp}`;
+    document.querySelector("footer p").textContent = footerContent;
+    
+    if (siteConfig.backgroundImage) {
+        body.style.backgroundImage = "url('img/background.jpg')";
+    }
+    
+    siteConfig.categories.forEach(category => {
+        const section = document.createElement("section");
+        
+        const h2 = document.createElement("h2");
+        h2.textContent = category.title;
+        section.appendChild(h2);
+
+        const cardContainer = document.createElement("div");
+        cardContainer.className = "card-container";
+
+        category.links.forEach(link => {
+            const card = document.createElement("a");
+            card.href = link.href;
+            card.className = "card";
+            card.title = link.description;
+
+            const imgContainer = document.createElement("div");
+            if (link.thumbnail) {
+                const img = document.createElement("img");
+                img.src = link.thumbnail;
+                imgContainer.appendChild(img);
+            } else {
+                const icon = document.createElement("i");
+                icon.className = "fa-solid fa-sitemap";
+                imgContainer.appendChild(icon);
+            }
+            card.appendChild(imgContainer);
+
+            const cardText = document.createElement("div");
+            cardText.className = "card-text";
+            cardText.textContent = link.text;
+            card.appendChild(cardText);
+
+            cardContainer.appendChild(card);
+        });
+
+        section.appendChild(cardContainer);
+
+        const divider = document.createElement("hr");
+        section.appendChild(divider);
+
+        main.appendChild(section);
+    });
+});
